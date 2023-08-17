8297a4997ebc1c52bce2c5ff3300600860a06ebc
coder-nian <nomahmad10@gmail.com>
2023-08-17 21:37:40 +0530

    Added comments in code.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Fetches data from API
 const getWeatherData = (city) => {
   let userCity = city
   const url = ''
@@ -9,12 +10,14 @@ const getWeatherData = (city) => {
   .catch(err => console.error(err))
 }
 
+//Takes user input
 const searchCity = async () => {
   const city = document.getElementById('city-input').value;
   const data = await getWeatherData(city)
   showWeatherData(data)
 }
 
+//Displays the data received from API 
 const showWeatherData = (weatherData) => {
   document.getElementById('temp').innerText = weatherData.main.temp
   document.getElementById('min-temp').innerText = weatherData.main.temp_min
